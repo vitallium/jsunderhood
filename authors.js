73174d79d1717e7fcfbd3c616c417e61628927c7
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2019-10-07 10:36:34 +0300

    Add @ovrweb

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ import authorId from './helpers/author-id';
 // prettier-ignore
 export default authorId([
   // post: false
+  { username: 'ovrweb'          , start: '07 Oct 2019', first: '1181109264238202881' },
   { username: 'kirjs'           , start: '01 Oct 2019', first: '1178917648060633088' },
   { username: 'zolotyh'         , start: '23 Sep 2019', first: '1176077272819929088' },
   { username: 'davert'          , start: '16 Sep 2019', first: '1173523382492684289' },
