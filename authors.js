d63b88319835e25c7f2767a64d60f9db8d6ca07e
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2019-12-17 09:46:03 +0300

    Add twit_iggy to authors

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ import authorId from './helpers/author-id';
 // prettier-ignore
 export default authorId([
   // post: false
+  { username: 'twit_iggy'       , start: '16 Dec 2019', first: '1206711262903705602' },
   { username: 'ta_fokina'       , start: '09 Dec 2019', first: '1204004528883937281' },
   { username: 'alekseikhlebaev' , start: '02 Dec 2019', first: '1201419806089633792' },
   { username: 'seirina_of_js'   , start: '25 Nov 2019', first: '1198882337347518464' },
