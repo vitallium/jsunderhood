543c5146d93d7ddb0f1def8e5d408ce3046014ff
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2020-05-25 12:49:02 +0300

    Add sem_eleven to authors

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ const authorId = require('./helpers/author-id');
 // prettier-ignore
 module.exports = authorId([
   // post: false
+  { username: 'sem_eleven'      , start: '25 May 2020', first: '1264854581684482048' },
   { username: 'mike1pol'        , start: '18 May 2020', first: '1262303295139446784' },
   { username: 'went_out'        , start: '27 Apr 2020', first: '1254659330529546240' },
   { username: 'GlafiraZhur'     , start: '20 Apr 2020', first: '1252158232036024320' },
