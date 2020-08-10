b66bd33ca4202084cc83eef101b64cf4be02ac72
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2020-08-10 16:24:16 +0300

    Add smelukov to authors

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ const authorId = require('./helpers/author-id');
 // prettier-ignore
 module.exports = authorId([
   // post: false
+  { username: 'smelukov'        , start: '10 Aug 2020', first: '1292741684086870016' },
   { username: 'deni_deniskina'  , start: '27 Jul 2020', first: '1287682044160548867' },
   { username: 'thewizardlucas'  , start: '06 Jul 2020', first: '1280043363543252992' },
   { username: '_sergeysova'     , start: '29 Jun 2020', first: '1277501157447958534' },
