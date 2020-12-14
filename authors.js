243fdb209cd1612b893df2acf0871975c579534a
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2020-12-14 10:51:13 +0300

    Add ShibaCodes to authors

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ const authorId = require('./helpers/author-id');
 // prettier-ignore
 module.exports = authorId([
   // post: false
+  { username: 'ShibaCodes'      , start: '14 Dec 2020', first: '1338387360686497792' },
   { username: 'rm_baad'         , start: '07 Dec 2020', first: '1335877650519306241' },
   { username: 'ilialesik'       , start: '30 Nov 2020', first: '1333319850182258688' },
   { username: 'dbezhetskov'     , start: '23 Nov 2020', first: '1330857456537362436' },
